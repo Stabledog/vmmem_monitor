063b0804ac5f326ed7181e1ee2350614142a8094
Les Matheson <les.matheson@gmail.com>
2023-09-16 20:03:15 -0400

    Hello world popup works

---
+++ src/build.bat
@@ -0,0 +1,3 @@
+@echo off
+cl /EHsc vmmem_monitor.c /link user32.lib
+pause
---
+++ src/vmmem_monitor.c
@@ -0,0 +1,6 @@
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nCmdShow) {
+    MessageBox(NULL, "Hello, World!", "Test", MB_OK);
+    return 0;
+}
