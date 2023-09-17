423b512706ccf30e93f40be38bceadba96b4ce41
Les Matheson <les.matheson@gmail.com>
2023-09-16 21:20:42 -0400

    Sync (auto)

---
+++ src/README.md
@@ -0,0 +1,11 @@
+# vmmem_monitor Project
+
+## Dev environment:
+    - Requires the free MS C++ Build Toolkit
+    - Use Windows Start menu to launch a Developer Command Prompt, and then `wsl.exe -- bash` in the project source dir.
+       (This is how WSL inherits the suitable environment vars to build)
+    - Within WSL shell, you can do `code .` and VSCode will then have the right environment also.
+    - Set up a build task to invoke `make`
+
+## Build:
+    -
--- src/vmmem_monitor.c
+++ src/vmmem_monitor.c
@@ -1,3 +1,4 @@
+
 #include <windows.h>
 
 #ifdef _0
@@ -15,9 +16,58 @@ void registerIcon(HWND hWnd)
 }
 #endif
 
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+    }
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    const char CLASS_NAME[] = "Sample Window Class";
+
+    WNDCLASSEX wc = {0};
+
+    wc.cbSize = sizeof(WNDCLASSEX);
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+
+    if (!RegisterClassEx(&wc)) {
+        MessageBox(NULL, "Window Registration Failed!", "Error!", MB_ICONEXCLAMATION | MB_OK);
+        return 0;
+    }
+
+    HWND hwnd = CreateWindowEx(
+        WS_EX_TOOLWINDOW, // make the window a tool window to hide it from the taskbar
+        CLASS_NAME,
+        "Sample Window",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+        NULL, NULL, hInstance, NULL
+    );
+
+    if (hwnd == NULL) {
+        MessageBox(NULL, "Window Creation Failed!", "Error!", MB_ICONEXCLAMATION | MB_OK);
+        return 0;
+    }
+
+    MSG msg = {0};
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return 0;
+}
+
+#ifdef _0
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nCmdShow)
 {
 
     MessageBox(NULL, "Hello, World!", "Test", MB_OK);
     return 0;
 }
+#endif
